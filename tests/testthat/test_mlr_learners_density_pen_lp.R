f1d6e5da216f72afab04f27e87f86ca97418348a
Ain Toha <nurul.toha.15@ucl.ac.uk>
2020-02-20 14:04:53 +0000

    edit np

--- tests/testthat/test_mlr_learners_density_pen_lp.R
+++ tests/testthat/test_mlr_learners_density_pen_lp.R
@@ -3,7 +3,7 @@ context("dens.penLP")
 test_that("autotest", {
   set.seed(50)
   learner = lrn("dens.penLP")
-  expect_learner(learner)
+  expect_learner(learner, N=50)
   result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
